ec24fd38ececdcc8244e181e37a6619c81d868f6
Zoljargal2004 <zolookorzoloo@gmail.com>
2025-01-17 19:53:06 +0800

    fireball prototype

--- scripts/fireball.gd
+++ scripts/fireball.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var speed = 1000
+var damage = 10
 var direction: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += speed * delta * direction
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_groups() == []:
+		return
+	if area.get_groups()[0] == "enemy":
+		fire_ball_explodes()
+	
+func fire_ball_explodes() -> void:
+	direction = Vector2(0,0)
+	queue_free()
