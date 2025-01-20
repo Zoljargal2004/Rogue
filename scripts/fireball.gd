cd5dd8f8a57eb558eda82ea7f9114c8b258de96d
MrPresent7 <Erkhembileg1230@gmail.com>
2025-01-20 16:08:46 +0000

    I an vest
    vandeiasel

--- scripts/fireball.gd
+++ scripts/fireball.gd
@@ -1,5 +1,9 @@
 extends Node2D
-var speed = 1000
+
+var explosion_scene = preload("res://scenes/explosion.tscn")
+@export var speed = 1000
+@export var damage: Node2D
+@export var explosion_collision: Node2D
 var direction: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +17,21 @@ func _process(delta: float) -> void:
 	
 func fire_ball_explodes() -> void:
 	direction = Vector2(0,0)
+	queue_free()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		explosion()
+	
+func explosion():
+	var exploding = explosion_scene.instantiate()
+	exploding.position = global_position
+	get_parent().add_child(exploding)
+	explosion_collision.set_deferred("disabled", false)
+
+func _on_explosion_area_entered(area: Area2D) -> void:
+
+	if area.has_method("damage"):
+		area.damage(damage.damage)
 	queue_free()
