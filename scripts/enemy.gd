b877dde49f2505a13c876d1da577496db44c1663
MrPresent7 <Erkhembileg1230@gmail.com>
2025-01-20 17:21:08 +0000

    Cgad
    Direction

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,17 +1,23 @@
 extends Node2D
 
+@export var Sprite:Node2D
 @export var speed = 100
 var food_scene = preload("res://scripts/food.gd")
+var scaler = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var scaler = Sprite.scale.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var player = get_parent().get_node("player")
 	var direction = (player.position - position).normalized()
-	
+	if direction.x <0:
+		Sprite.scale.x = -scaler
+	else:
+		Sprite.scale.x = scaler
+		
 	position += direction * speed * delta
 
 func enemy_die() -> void:
