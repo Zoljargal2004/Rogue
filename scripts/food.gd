a1a83d86f5a4cfdb4b4fc5a665b9971e0f05f3d9
Zoljargal2004 <zolookorzoloo@gmail.com>
2025-01-22 00:59:48 +0800

    xp gain

--- scripts/food.gd
+++ scripts/food.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var xp = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,5 +17,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		return
 	if area.get_groups()[0] == "foodPicker":
 		var level = get_parent().get_child(0).get_node("levelSystem")
-		level.current_xp += 1
+		level.current_xp += xp
 		queue_free()
